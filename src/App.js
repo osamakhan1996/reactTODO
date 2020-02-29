42afb330365519378566b96037ee2f9838ca84bb
Muhammad Osama Khan <khanosama1996@gmail.com>
2020-02-29 14:39:58 +0500

    Create

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ListItem from './ListItem'
+
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state={
+      items:[],
+      currentItem:{
+        text:'',
+        key:''
+      }
+     
+    }
+    this.handleInput = this.handleInput.bind(this)
+    this.addItem = this.addItem.bind(this)
+  }
+  handleInput(e){
+    this.setState({
+      currentItem:{
+        text: e.target.value,
+        key: Date.now()
+      }
+    })
+  }
+  addItem(e){
+    e.preventDefault();
+    const newItem = this.state.currentItem;
+    console.log(newItem)
+    if(newItem.text!==" "){
+      const newItems = [...this.state.items ,newItem]
+      this.setState({
+        items: newItems,
+        currentItem: {
+          text:'',
+          key:''
+        }
+      })
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <header>
+        <form id="to-do-form" onSubmit={this.addItem}>
+          <input type="text" placeholder="Enter text" value={this.state.currentItem.text} onChange={this.handleInput} />
+          <button type="submit">Add</button>
+        </form>
+      </header>
+      <ListItem items = {this.state.items}/>
+      </div>
+    );
+  }
+}
+
+export default App;
